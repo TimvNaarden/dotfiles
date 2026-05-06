7c834ed5c4fc11a300dba0e6e96fcfa00cdb5884
Tim van Naarden <TimvNaarden@outlook.com>
2026-05-06 16:29:14 +0200

    CPP  debugger

--- .config/nvim/lua/custom/configs/dap.lua
+++ .config/nvim/lua/custom/configs/dap.lua
@@ -21,3 +21,17 @@ for _, language in ipairs { "typescript", "javascript" } do
     },
   }
 end
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+  },
+}
+dap.configurations.c = dap.configurations.cpp
